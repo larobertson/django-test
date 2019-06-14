70fe82f808a89c8aaec1c9d542bbe67eb27a518e
Laura <laura.robertson64@gmail.com>
2019-06-14 13:56:43 -0500

    fixed bug, imported Choice

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
