fc0eaabacd46700c7ecebd8cebbfd0147d444c62
Laura <laura.robertson64@gmail.com>
2019-06-14 13:00:22 -0500

    replaced detail try and except with shortcut for render or 404

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Question
 
@@ -13,10 +13,7 @@ def index(request):
   # HttpResponse object of the given template rendered with the given context.
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
+  question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
